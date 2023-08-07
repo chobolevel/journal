9876f01835096407ba6c5866d046612082b2a942
chobolevel <rodaka123@naver.com>
2023-08-07 16:39:10 +0900

    feat: add Security Setting

---
+++ src/main/java/kr/co/space/diary/security/entity/UserDetailsImpl.java
@@ -0,0 +1,54 @@
+package kr.co.space.diary.security.entity;
+
+import kr.co.space.diary.entity.member.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Getter
+@AllArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private Member member;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collect = new ArrayList<>();
+        collect.add((GrantedAuthority) () -> member.getRole());
+        return collect;
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
