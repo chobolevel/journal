afd4b21b4b4317d40f21e9ca44309b00c93dcf8e
chobolevel <rodaka123@naver.com>
2023-08-08 16:17:25 +0900

    refactor: modify create method
    add validation

--- src/main/java/kr/co/space/diary/service/member/impl/MemberServiceImpl.java
+++ src/main/java/kr/co/space/diary/service/member/impl/MemberServiceImpl.java
@@ -2,10 +2,12 @@ package kr.co.space.diary.service.member.impl;
 
 import kr.co.space.diary.entity.member.Member;
 import kr.co.space.diary.enums.common.CustomExceptionType;
+import kr.co.space.diary.enums.member.MemberRoleType;
 import kr.co.space.diary.exception.CustomException;
 import kr.co.space.diary.mapper.member.MemberMapper;
 import kr.co.space.diary.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,6 +18,7 @@ import java.util.UUID;
 public class MemberServiceImpl implements MemberService {
 
   private final MemberMapper memberMapper;
+  private final BCryptPasswordEncoder passwordEncoder;
 
   @Override
   public List<Member> findAll(Member member) {
@@ -38,7 +41,15 @@ public class MemberServiceImpl implements MemberService {
     else if(member.getPassword() == null || member.getPassword().isEmpty()) {
       throw new CustomException(CustomExceptionType.MISSING_PARAMETER, "String", "password");
     }
+    else if(member.getName() == null || member.getName().isEmpty()) {
+      throw new CustomException(CustomExceptionType.MISSING_PARAMETER, "String", "name");
+    }
+    else if(member.getNickname() == null || member.getNickname().isEmpty()) {
+      throw new CustomException(CustomExceptionType.MISSING_PARAMETER, "String", "nickname");
+    }
     member.setId(UUID.randomUUID().toString());
+    member.setPassword(passwordEncoder.encode(member.getPassword()));
+    member.setRole(MemberRoleType.ROLE_USER);
     memberMapper.create(member);
   }
 
