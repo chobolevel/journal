295e7810aef044391449a15744eed21b5f23d093
chobolevel <rodaka123@naver.com>
2023-08-08 16:19:28 +0900

    feat: add UserDetails embodiment

---
+++ src/main/java/kr/co/space/diary/config/security/principal/PrincipalDetails.java
@@ -0,0 +1,54 @@
+package kr.co.space.diary.config.security.principal;
+
+import kr.co.space.diary.entity.member.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Getter
+@AllArgsConstructor
+public class PrincipalDetails implements UserDetails {
+
+  private Member member;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    Collection<GrantedAuthority> collect = new ArrayList<>();
+    collect.add((GrantedAuthority) () -> member.getRole().getName());
+    return collect;
+  }
+
+  @Override
+  public String getPassword() {
+    return member.getPassword();
+  }
+
+  @Override
+  public String getUsername() {
+    return member.getUsername();
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+}
