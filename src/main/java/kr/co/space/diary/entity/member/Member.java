474097f5521055ec4f79e502023a4008d993034a
chobolevel <rodaka123@naver.com>
2023-08-14 15:47:03 +0900

    refactor: password 필드 어노테이션 변경
    password field에 대해 setter만 사용할 수 있도록 수정
    정보를 조회할 때에 password field는 제공하지 않도록 수정

--- src/main/java/kr/co/space/diary/entity/member/Member.java
+++ src/main/java/kr/co/space/diary/entity/member/Member.java
@@ -1,6 +1,6 @@
 package kr.co.space.diary.entity.member;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import kr.co.space.diary.entity.common.Base;
 import kr.co.space.diary.enums.member.MemberRoleType;
 import lombok.*;
@@ -14,7 +14,7 @@ public class Member extends Base {
 
     private String id;
     private String username;
-    @JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     private String name;
     private String nickname;
