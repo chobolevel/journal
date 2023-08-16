60f4c31a8c473b21af3f1cbf3a620afb16a46889
chobolevel <rodaka123@naver.com>
2023-08-16 16:46:57 +0900

    feat: 조회수 중복 상승 제어 기능 추가

--- src/main/java/kr/co/space/diary/controller/diary/DiaryRestController.java
+++ src/main/java/kr/co/space/diary/controller/diary/DiaryRestController.java
@@ -1,5 +1,6 @@
 package kr.co.space.diary.controller.diary;
 
+import kr.co.space.diary.config.security.principal.PrincipalDetails;
 import kr.co.space.diary.entity.attachment.Attachment;
 import kr.co.space.diary.entity.diary.Diaries;
 import kr.co.space.diary.entity.diary.Diary;
@@ -8,12 +9,15 @@ import kr.co.space.diary.service.attachment.AttachmentService;
 import kr.co.space.diary.service.diary.DiaryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -23,6 +27,7 @@ public class DiaryRestController {
 
   private final DiaryService diaryService;
   private final AttachmentService attachmentService;
+  private final RedisTemplate<String, String> redisTemplate;
 
   @Value("${spring.servlet.multipart.location}")
   private String basePath;
@@ -66,8 +71,29 @@ public class DiaryRestController {
   }
 
   @PutMapping("increase-view-cnt/{id}")
-  public HttpStatus increaseViewCnt(@PathVariable("id") String id) throws CustomException {
-    diaryService.increaseViewCnt(Diary.builder().id(id).build());
+  public HttpStatus increaseViewCnt(@AuthenticationPrincipal PrincipalDetails principalDetails, @PathVariable("id") String id) throws CustomException {
+    String memberId = principalDetails.getMember().getId();
+    String viewedDiaryIds = redisTemplate.opsForValue().get(memberId);
+    List<String> viewedDiaryIdList = new ArrayList<>();
+    if(viewedDiaryIds == null) {
+      // 없을 땐 새롭게 key:value 생성 -> 조회수 상승
+      redisTemplate.opsForValue().set(memberId, id);
+      diaryService.increaseViewCnt(Diary.builder().id(id).build());
+    }
+    else {
+      // null이 아니면서 조회한 아이디에 포함되는 경우 아닌 경우 구분
+      for(String s : viewedDiaryIds.split(", ")) {
+        viewedDiaryIdList.add(s);
+      }
+      if(viewedDiaryIdList.contains(id)) {
+        // 매칭되는 경우 -> 조회 수 상승 X
+      } else {
+        // 매칭되지 않는 경우
+        viewedDiaryIdList.add(1, id);
+        redisTemplate.opsForValue().set(memberId, String.join(", ", viewedDiaryIdList));
+        diaryService.increaseViewCnt(Diary.builder().id(id).build());
+      }
+    }
     return HttpStatus.OK;
   }
 
