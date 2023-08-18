27781a597b2b9aec0b9fbb5b43b82da1a55fc5e5
chobolevel <rodaka123@naver.com>
2023-08-18 20:35:16 +0900

    refactor: 일기 수정 화면 script 수정
    model 대신 api 사용하도록 수정 및 URL 수정

--- src/main/java/kr/co/space/diary/controller/diary/DiaryController.java
+++ src/main/java/kr/co/space/diary/controller/diary/DiaryController.java
@@ -1,15 +1,12 @@
 package kr.co.space.diary.controller.diary;
 
-import kr.co.space.diary.entity.diary.Diary;
 import kr.co.space.diary.exception.CustomException;
 import kr.co.space.diary.service.attachment.AttachmentService;
 import kr.co.space.diary.service.diary.DiaryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -38,10 +35,8 @@ public class DiaryController {
     return "/diary/watch";
   }
 
-  @GetMapping("modify/{id}")
-  public String modify(@PathVariable("id") String id, Model model) throws CustomException {
-    Diary findDiary = diaryService.findOne(Diary.builder().id(id).build());
-    model.addAttribute("diary", findDiary);
+  @GetMapping("modify")
+  public String modify() throws CustomException {
     return "/diary/modify";
   }
 
