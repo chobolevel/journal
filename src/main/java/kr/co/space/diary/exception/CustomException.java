381dba2f5a9762f8426307e7828a8425e12de95b
chobolevel <rodaka123@naver.com>
2023-09-02 16:51:25 +0900

    Refactor: CustomException
    @Getter 어노테이션 추가 및 기존 메서드 삭제
    CustomExceptionType만 받는 생성자 추가

--- src/main/java/kr/co/space/diary/exception/CustomException.java
+++ src/main/java/kr/co/space/diary/exception/CustomException.java
@@ -1,7 +1,9 @@
 package kr.co.space.diary.exception;
 
 import kr.co.space.diary.enums.common.CustomExceptionType;
+import lombok.Getter;
 
+@Getter
 public class CustomException extends Exception{
 
   private final CustomExceptionType type;
@@ -9,22 +11,12 @@ public class CustomException extends Exception{
   private String[] args;
 
   public CustomException(CustomExceptionType type) {
-    super(type.getMessage());
     this.type = type;
   }
 
   public CustomException(CustomExceptionType type, String... args) {
-    super(type.getMessage());
     this.type = type;
     this.args = args;
-  }
-
-  public CustomExceptionType getType() {
-    return type;
-  }
-
-  public String[] getArgs() {
-    return args;
   }
 
 }
