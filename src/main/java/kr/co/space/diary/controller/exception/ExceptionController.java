f024f7c0cdd3feddb513f50e2b9c62606ac29f79
chobolevel <rodaka123@naver.com>
2023-09-03 16:30:50 +0900

    Refactor: remove ExceptionHandler
    API 완성 이후 재작성 예정

--- src/main/java/kr/co/space/diary/controller/exception/ExceptionController.java
+++ src/main/java/kr/co/space/diary/controller/exception/ExceptionController.java
@@ -1,15 +1,8 @@
 package kr.co.space.diary.controller.exception;
 
-import kr.co.space.diary.exception.CustomException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ExceptionController {
-
-  @ExceptionHandler(CustomException.class)
-  public String error() {
-    return "/exception/error";
-  }
 
 }
