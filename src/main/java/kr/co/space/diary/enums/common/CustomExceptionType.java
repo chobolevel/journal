be2e23f32debfedaee90ff92437bf28fa82f3196
chobolevel <rodaka123@naver.com>
2023-09-03 15:42:37 +0900

    Refactor: add CustomExceptionType
    조회 실패한 경우 예외 타입 추가

--- src/main/java/kr/co/space/diary/enums/common/CustomExceptionType.java
+++ src/main/java/kr/co/space/diary/enums/common/CustomExceptionType.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 public enum CustomExceptionType {
 
   MISSING_PARAMETER(HttpStatus.BAD_REQUEST, "#0[Type: #1]이/가 누락되었습니다."),
+  FAIL_TO_FIND(HttpStatus.BAD_REQUEST, "요청하는 정보를 불러오지 못했습니다."),
   NOT_MATCH_PASSWORD(HttpStatus.BAD_REQUEST, "기존 비밀번호와 일치하지 않습니다.");
 
 
