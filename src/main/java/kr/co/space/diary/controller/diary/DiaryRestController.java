7bae2a43d3d5f140541173e6fea79c0d28637259
chobolevel <rodaka123@naver.com>
2023-09-10 15:02:51 +0900

    Refactor: increase-view-cnt logic 기존 레디스 활용하던 로직 쿠키 사용하도록 수정

--- src/main/java/kr/co/space/diary/controller/diary/DiaryRestController.java
+++ src/main/java/kr/co/space/diary/controller/diary/DiaryRestController.java
@@ -1,21 +1,20 @@
 package kr.co.space.diary.controller.diary;
 
-import kr.co.space.diary.config.security.principal.PrincipalDetails;
 import kr.co.space.diary.entity.common.HttpResult;
 import kr.co.space.diary.entity.diary.Diaries;
 import kr.co.space.diary.entity.diary.Diary;
 import kr.co.space.diary.exception.CustomException;
 import kr.co.space.diary.service.diary.DiaryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -24,7 +23,6 @@ import java.util.List;
 public class DiaryRestController {
 
   private final DiaryService diaryService;
-  private final RedisTemplate<String, String> redisTemplate;
 
   @GetMapping("{id}")
   public ResponseEntity<?> fetch(@PathVariable("id") String id) throws CustomException {
@@ -55,27 +53,20 @@ public class DiaryRestController {
   }
 
   @PutMapping("increase-view-cnt/{id}")
-  public ResponseEntity<?> increaseViewCnt(@AuthenticationPrincipal PrincipalDetails principalDetails, @PathVariable("id") String id) throws CustomException {
-    String memberId = principalDetails.getMember().getId();
-    String viewedDiaryIds = redisTemplate.opsForValue().get(memberId);
-    List<String> viewedDiaryIdList = new ArrayList<>();
+  public ResponseEntity<?> increaseViewCnt(HttpServletResponse res,
+                                           @CookieValue(value = "viewedDiaryIds", required = false) String viewedDiaryIds,
+                                           @PathVariable("id") String id) throws CustomException {
     if(viewedDiaryIds == null) {
-      // 없을 땐 새롭게 key:value 생성 -> 조회수 상승
-      redisTemplate.opsForValue().set(memberId, id);
       diaryService.increaseViewCnt(Diary.builder().id(id).build());
-    }
-    else {
-      // null이 아니면서 조회한 아이디에 포함되는 경우 아닌 경우 구분
-      for (String s : viewedDiaryIds.split(", ")) {
-        viewedDiaryIdList.add(s);
-      }
-      if (viewedDiaryIdList.contains(id)) {
-        // 매칭되는 경우 -> 조회 수 상승 X
-      } else {
-        // 매칭되지 않는 경우
-        viewedDiaryIdList.add(1, id);
-        redisTemplate.opsForValue().set(memberId, String.join(", ", viewedDiaryIdList));
+      Cookie cookie = new Cookie("viewedDiaryIds", id);
+      res.addCookie(cookie);
+    } else {
+      List<String> viewedDiaryIdList = Arrays.asList(viewedDiaryIds.split("/"));
+      if(!viewedDiaryIdList.contains(id)) {
+        // 조회수 상승 및 쿠키에 아이디 저장
         diaryService.increaseViewCnt(Diary.builder().id(id).build());
+        Cookie cookie = new Cookie("viewedDiaryIds", viewedDiaryIds + "/" + id);
+        res.addCookie(cookie);
       }
     }
     return ResponseEntity.ok(HttpResult.ok("success to increase view count"));
